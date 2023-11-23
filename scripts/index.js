4212480a10fac163d60d5ceffb74ee85eec7eff4
Ilyes Soumar Djouma <isoumardjouma@reseau.free.fr>
2023-11-24 00:35:11 +0100

    test algo

--- scripts/index.js
+++ scripts/index.js
@@ -19,19 +19,46 @@ async function getRecipes() {
 
 
 async function displayData(recipes) {
-  const recipesSection = document.querySelector(".card-recipe-container");
+    const recipesSection = document.querySelector(".card-recipe-container");
 
-  recipes.forEach((recipe) => {
-    const recipeModel = recipesTemplate(recipe);
-    const recipeCardDOM = recipeModel.getRecipeCardDOM();
-    recipesSection.appendChild(recipeCardDOM);
-  });
+    recipes.forEach((recipe) => {
+        const recipeModel = recipesTemplate(recipe);
+        const recipeCardDOM = recipeModel.getRecipeCardDOM();
+        recipesSection.appendChild(recipeCardDOM);
+    });
 }
 
 async function init() {
-  // Récupère les datas des photographes
-  const { recipes } = await getRecipes();
-  displayData(recipes);
+    // Récupère les datas des photographes
+    const { recipes } = await getRecipes();
+    displayData(recipes);
 }
 
 init();
+
+const searchBar = document.getElementById("searchbar");
+
+searchBar.addEventListener("input", function(event){
+    const regExp = /^[a-zA-Z]{3,}$/;
+    const match = regExp.test(event.target.value);
+    if (match) {
+      console.log("palier 1");
+      const regExp = /coco/i;;
+      const testCoco = regExp.test(event.target.value);
+      const tempRecipeArray = [];
+          if (testCoco) {
+              console.log("palier 2");
+              console.log(recipesList[0].name);
+              console.log(recipesList[0].name.includes("coco"));
+              recipesList.forEach((recipe) => {
+                  const name = recipe.name;
+                  for (i = 0 ; i < recipesList.length; i++) {
+                      if (recipe[i].includes("coco")) {
+                          tempRecipeArray.push(recipe[i])
+                      }
+                      console.log(tempRecipeArray);
+                  }
+              })
+          }
+    }
+})
