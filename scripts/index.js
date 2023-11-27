a2d820fbee75408b00f2035dcf40d1320cf655eb
Ilyes Soumar Djouma <isoumardjouma@reseau.free.fr>
2023-11-27 22:44:15 +0100

    code review

--- scripts/index.js
+++ scripts/index.js
@@ -8,9 +8,9 @@ async function getRecipes() {
     } else {
         let data = await response.json();
         let recipes = data.recipes;
-        console.log(recipes);
+        // console.log(recipes);
         recipesList = [...recipes];
-        console.log(recipes[2].ingredients)
+        // console.log(recipes[2].ingredients)
         return {
           recipes: [...recipes],
         };
@@ -39,26 +39,29 @@ init();
 const searchBar = document.getElementById("searchbar");
 
 searchBar.addEventListener("input", function(event){
+    const recipesSection = document.querySelector(".card-recipe-container");
+    const suggestions = document.querySelector(".suggestions");
     const regExp = /^[a-zA-Z]{3,}$/;
     const match = regExp.test(event.target.value);
-    if (match) {
-      console.log("palier 1");
-      const regExp = /coco/i;;
-      const testCoco = regExp.test(event.target.value);
-      const tempRecipeArray = [];
-          if (testCoco) {
-              console.log("palier 2");
-              console.log(recipesList[0].name);
-              console.log(recipesList[0].name.includes("coco"));
-              recipesList.forEach((recipe) => {
-                  const name = recipe.name;
-                  for (i = 0 ; i < recipesList.length; i++) {
-                      if (recipe[i].includes("coco")) {
-                          tempRecipeArray.push(recipe[i])
-                      }
-                      console.log(tempRecipeArray);
-                  }
-              })
-          }
-    }
+    const searchIcon = document.getElementById("search-icon");
+    suggestions.textContent = "";
+    searchIcon.addEventListener("click", function() {
+      if (event.target.value === "") {
+        displayData(recipesList);
+      }
+        if (match) {
+          const tempRecipeArray = [];
+          console.log("palier 1")
+          recipesList.forEach((recipe) => {
+              const name = recipe.name;
+              if (name.toLowerCase().includes(`${event.target.value}`)) {
+                  tempRecipeArray.push(recipe)
+                  suggestions.innerHTML = `
+                  <li>${name}</li>`;
+              }
+          })
+              recipesSection.innerHTML = "";
+              displayData(tempRecipeArray)
+        }
+    })
 })
