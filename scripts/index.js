19f9e81cad6b1f22335c7819178fbe96f6afc25d
Ilyes Soumar Djouma <isoumardjouma@reseau.free.fr>
2023-12-04 00:55:58 +0100

    filter nav

--- scripts/index.js
+++ scripts/index.js
@@ -54,14 +54,32 @@ searchBar.addEventListener("input", function(event){
           console.log("palier 1")
           recipesList.forEach((recipe) => {
               const name = recipe.name;
-              if (name.toLowerCase().includes(`${event.target.value}`)) {
+              const ustenstil = recipe.ustentils;
+              const description = recipe.description;
+              if (name.toLowerCase().includes(`${event.target.value}`) || description.toLowerCase().includes(`${event.target.value}`)) {
                   tempRecipeArray.push(recipe)
                   suggestions.innerHTML = `
-                  <li>${name}</li>`;
+                  <li>${recipe.id}</li>`;
               }
           })
               recipesSection.innerHTML = "";
               displayData(tempRecipeArray)
         }
+    })
+})
+
+const filterIngredients = document.getElementById("filter-ingredients");
+const ingredientsIcon = document.getElementById("ingredients-icon");
+const filterHidden = document.querySelector(".filter-hidden");
+
+filterIngredients.addEventListener("click", function(){
+    ingredientsIcon.classList.toggle("fa-angle-down");
+    ingredientsIcon.classList.toggle("fa-angle-up");
+    filterHidden.classList.toggle("filter-hidden");
+    filterHidden.classList.toggle("filter-visible");
+    recipesList.forEach((recipe) => {
+        const name = recipe.name;
+        const ingredient = recipe.ingredients[ingredient];
+        console.log(ingredient);
     })
 })
