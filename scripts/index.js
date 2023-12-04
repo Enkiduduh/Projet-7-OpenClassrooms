3e868d3874c01446be9fd7bd7ef789c99f3b2eb7
Ilyes Soumar Djouma <isoumardjouma@reseau.free.fr>
2023-12-04 16:15:44 +0100

    code review

--- scripts/index.js
+++ scripts/index.js
@@ -35,8 +35,38 @@ async function init() {
 }
 
 init();
+console.log(recipesList);
+
+// function searchInRecipes(arrayOfRecipes, input) {
+//     const temporyRecipesArr = [];
+//     console.log("1");
+//     if (input.length >= 3) {
+//       console.log("2");
+//         arrayOfRecipes.forEach(recipe => {
+//             const lowerInput = input.toLowerCase();
+//             console.log("3");
+//             if (recipe.name.toLowerCase().includes(lowerInput) ||
+//                 recipe.description.toLowerCase().includes(lowerInput) ||
+//                 recipe.ingredients.some(ingredient => ingredient.ingredient.toLowerCase().includes(lowerInput))
+//             ) {
+//                 temporyRecipesArr.push(recipe);
+//                 console.log("4");
+//                 console.log(temporyRecipesArr);
+//                 console.log("5");
+//             }
+//             console.log("6");
+//         });
+//         console.log("7");
+//         displayData(temporyRecipesArr);
+//         console.log("8");
+//       }
+// }
 
 const searchBar = document.getElementById("searchbar");
+// searchBar.addEventListener("input", function(){
+//   console.log("Input event triggered");
+//     searchInRecipes(recipesList, searchBar.value);
+// });
 
 searchBar.addEventListener("input", function(event){
     const recipesSection = document.querySelector(".card-recipe-container");
@@ -71,15 +101,30 @@ searchBar.addEventListener("input", function(event){
 const filterIngredients = document.getElementById("filter-ingredients");
 const ingredientsIcon = document.getElementById("ingredients-icon");
 const filterHidden = document.querySelector(".filter-hidden");
+const filterList = document.querySelector(".filter-list");
+const ingredientsArr = [];
+
+const InputFilterIngredients = document.getElementById("filter-ingredients-input");
 
 filterIngredients.addEventListener("click", function(){
     ingredientsIcon.classList.toggle("fa-angle-down");
     ingredientsIcon.classList.toggle("fa-angle-up");
     filterHidden.classList.toggle("filter-hidden");
     filterHidden.classList.toggle("filter-visible");
+    filterList.innerHTML = "";
     recipesList.forEach((recipe) => {
         const name = recipe.name;
-        const ingredient = recipe.ingredients[ingredient];
-        console.log(ingredient);
+        if (recipe.ingredients && Array.isArray(recipe.ingredients)) {
+            recipe.ingredients.forEach((ingredient) => {
+                if (ingredient && ingredient.ingredient) {
+                  ingredientsArr.push(ingredient.ingredient);
+                }
+            });
+        }
     })
+    console.log(ingredientsArr);
+    const arrayIngredientsUniques = [...new Set(ingredientsArr)];
+    for (let i = 0; i < arrayIngredientsUniques.length; i++) {
+        filterList.innerHTML +=`<span>${arrayIngredientsUniques[i]}</span>`;
+    }
 })
